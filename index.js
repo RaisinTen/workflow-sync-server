0f92e6dd8886440a8afd9e4a1aee3f4a26737d3a
raisinten <raisinten@gmail.com>
2020-12-05 19:22:22 +0530

    src: add base of express server

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-const http = require('http');
-const port = process.env.PORT || 5000;
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>web sync server</h1>');
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+app.listen(5000 || process.env.PORT);
