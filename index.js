ba12ef70696b2c08e651999641145462e12a7dd1
raisinten <raisinten@gmail.com>
2020-12-09 20:37:25 +0530

    src: remove unnecessary timeout assignment

--- index.js
+++ index.js
@@ -104,10 +104,6 @@ app.delete('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT);
-
-server.timeout = 0;
-
-console.log(`Server timeout: ${ server.timeout }`);
+app.listen(PORT);
 
 console.log(`Listening on port ${PORT}`);
